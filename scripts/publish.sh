2fb32b8f4a5d7deeb5fe049849b111ef22c9e6b9
Eugene Dementjev <drdaemos@gmail.com>
2016-07-06 14:56:48 +0400

    [!] fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,11 +13,13 @@ source=$5
 build=$6
 
 # Set the path of the hosted site
-site="/home/ubuntu/www/$repo"
+site="/home/ubuntu/www"
 
 # Remove old site files, move new ones in place
 # On amazon EC2 use sudo if nginx html forlder has root ownership
 
 shopt -s extglob
-rm -rf "$site/!(*html*|*info*)"
-mv $build $site
+echo "$build | $site"
+rm -rfv $site/!(*html*|*info*)
+mkdir $site/$repo
+mv $build/* $site/$repo/
